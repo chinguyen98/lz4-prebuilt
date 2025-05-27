cea7f13ff36bfda3d867967f991db6661cfb9794
chinguyen <dacchi6761@gmail.com>
2025-05-27 16:22:07 +0700

    Fix binary module loading paths

--- dist/binary/index.d.ts
+++ dist/binary/index.d.ts
@@ -1,2 +1,6 @@
+declare const path: any;
+declare const fs: any;
+declare function findBinary(): any;
+declare const binaryPath: any;
 declare const lz4Binary: any;
 //# sourceMappingURL=index.d.ts.map
--- dist/binary/index.js
+++ dist/binary/index.js
@@ -1,7 +1,23 @@
 "use strict";
-const lz4Binary = require('./lz4.node');
+const path = require('path');
+const fs = require('fs');
+function findBinary() {
+    const possiblePaths = [
+        path.join(__dirname, 'lz4.node'), // Development path
+        path.join(__dirname, '..', '..', 'lib', 'binary', 'lz4.node'), // Source path
+        path.join(__dirname, '..', 'binary', 'lz4.node') // Production path
+    ];
+    for (const binaryPath of possiblePaths) {
+        if (fs.existsSync(binaryPath)) {
+            return binaryPath;
+        }
+    }
+    throw new Error(`Failed to find LZ4 binary module. Searched paths: ${possiblePaths.join(', ')}`);
+}
+const binaryPath = findBinary();
+const lz4Binary = require(binaryPath);
 if (!lz4Binary || typeof lz4Binary.encode !== 'function' || typeof lz4Binary.decode !== 'function') {
-    throw new Error('Failed to load LZ4 binary module');
+    throw new Error('Failed to load LZ4 binary module - invalid module format');
 }
 module.exports = lz4Binary;
 //# sourceMappingURL=index.js.map
